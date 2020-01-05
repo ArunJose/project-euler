652852ef256442d30dc32c49c05a7ee3281481ce
Arun Jose <12997751+ArunJose@users.noreply.github.com>
2020-01-05 06:07:10 +0530

    Refactor problem 2 solution

--- problem-2-even-fibonacci-numbers.js
+++ problem-2-even-fibonacci-numbers.js
@@ -2,7 +2,7 @@ function fiboEvenSum(n) {
   let prevNum = 0, currNum = 1, sumOfEven = 0;
   for (let i = 0; i < n; i++) {
     let temp = currNum;
-    currNum = currNum + prevNum;
+    currNum += prevNum;
     prevNum = temp;
     sumOfEven += currNum % 2 === 0 && currNum;
   }
